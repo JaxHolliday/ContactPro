82346df37d1f3f36dadbda403f1a375540a50fba
Jackson Holliday <jaxholliday@gmail.com>
2022-10-18 10:35:59 -0400

    Mail settings/packages setup

---
+++ ContactPro/Models/MailSettings.cs
@@ -0,0 +1,16 @@
+﻿namespace ContactPro.Models
+{
+    public class MailSettings
+    {
+        //this class mimics out mail settings info made in secrets
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? DisplayName { get; set; }
+
+        public string? Host { get; set; }
+
+        public int Port { get; set; }
+    }
+}
