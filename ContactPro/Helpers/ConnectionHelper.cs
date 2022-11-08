8d3228c0c54c7cb12301b910727dd9d26551a8d1
Jackson Holliday <jaxholliday@gmail.com>
2022-11-08 10:56:12 -0500

    Connection Helper (Local vs Production)

---
+++ ContactPro/Helpers/ConnectionHelper.cs
@@ -0,0 +1,41 @@
+﻿using Npgsql;
+
+namespace ContactPro.Helpers
+{
+    //static - can only be one , dont haft to create instance
+    //Static == class that cannot be instantiated 
+    //making static so we dont haft to create instance 
+    public static class ConnectionHelper
+    {
+        public static string GetConnectionString(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetSection("pgSettings")["pgConnection"];
+            //checks environment that we go to
+            var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+            return String.IsNullOrEmpty(databaseUrl) ? connectionString : BuildConnectionString(databaseUrl);
+        }
+
+        //building connection string from the environment. i.e heroku
+        private static string BuildConnectionString(string databaseUrl)
+        {
+            //Universal resource identity 
+            var databaseUri = new Uri(databaseUrl);
+            //split into array to access individually
+            var userInfo = databaseUri.UserInfo.Split(':');
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = databaseUri.Host,
+                Port = databaseUri.Port,
+                Username = userInfo[0],
+                Password = userInfo[1],
+                Database = databaseUri.LocalPath.TrimStart('/'),
+                SslMode = SslMode.Require,
+                TrustServerCertificate = true
+            };
+
+            return builder.ToString();  
+
+        }
+    }
+}
--- ContactPro/Program.cs
+++ ContactPro/Program.cs
@@ -1,4 +1,5 @@
 using ContactPro.Data;
+using ContactPro.Helpers;
 using ContactPro.Models;
 using ContactPro.Services;
 using ContactPro.Services.Interfaces;
@@ -10,7 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+//var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
